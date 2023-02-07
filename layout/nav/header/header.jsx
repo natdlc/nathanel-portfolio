337bac5f8836d2c6d3b41dcf20d0f911f60ce73f
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-07 16:04:05 +0800

    wip responsiveness again

--- layout/nav/header/header.jsx
+++ layout/nav/header/header.jsx
@@ -1,14 +1,7 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
 import styles from "./header.module.css";
-import Anchor from "@/components/common/anchor/anchor";
-import Submenu from "@/components/common/submenu/submenu";
-import { getAboutArticles } from "@/lib/content/about";
-import { getWorkItems } from "@/lib/content/work";
 import MobileNav from "./mobile-nav/mobile-nav";
 import { getMainMenu } from "@/lib/content/nav";
 import CustomLink from "@/components/common/custom-link/custom-link";
-import useSubmenu from "@/hooks/useSubmenu";
 
 const mainMenu = getMainMenu();
 
@@ -16,6 +9,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <p className={styles.logo}>nathanel.dev</p>
+      <MobileNav />
       <nav className={styles.nav}>
         <ul className={styles.ul}>
           {mainMenu.map((link) => (
--- layout/nav/header/mobile-nav/mobile-nav.jsx
+++ layout/nav/header/mobile-nav/mobile-nav.jsx
@@ -1,30 +1,9 @@
-import Anchor from "@/components/common/anchor/anchor";
-import Submenu from "@/components/common/submenu/submenu";
-import useSubmenu from "@/hooks/useSubmenu";
-import { getAboutArticles } from "@/lib/content/about";
-import { getWorkItems } from "@/lib/content/work";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import styles from "./mobile-nav.module.css";
 
 export default function MobileNav() {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-
-  const router = useRouter();
-  const isNotHome = router.pathname !== "/";
-
-  const aboutSubmenu = useSubmenu({
-    path: "about",
-    title: "About",
-    items: getAboutArticles(),
-  });
-
-  const workSubmenu = useSubmenu({
-    path: "work",
-    title: "Work",
-    items: getWorkItems(),
-  });
 
   return (
     <nav className={styles.nav}>
@@ -34,27 +13,7 @@ export default function MobileNav() {
           isMobileNavOpen ? styles.open : ""
         }`}
       ></button>
-      {isMobileNavOpen ? (
-        <ul className={styles.ul}>
-          <li>
-            <Link scroll={false} href="/" className={styles.link}>
-              Home
-            </Link>
-          </li>
-
-          <Submenu submenu={aboutSubmenu} isMobile />
-          <Submenu submenu={workSubmenu} isMobile />
-
-          <li>
-            <Link href="blog" className={styles.link}>
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Anchor href={isNotHome ? "/#contact" : "#contact"}>Contact</Anchor>
-          </li>
-        </ul>
-      ) : null}
+      {isMobileNavOpen ? <ul className={styles.ul}></ul> : null}
     </nav>
   );
 }
