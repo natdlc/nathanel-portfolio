53a18fa999ac339d5c04336b015bf0b260008b50
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-08 13:47:03 +0800

    skills - hero - mobile responsive

--- components/skills/skills.jsx
+++ components/skills/skills.jsx
@@ -1,6 +1,4 @@
 import { getSkillItems } from "@/lib/content/skills";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import Hero from "./hero/hero";
 import Skill from "./skill/skill";
 
@@ -10,7 +8,6 @@ export default function SkillsPage({ dispatch }) {
   return (
     <>
       <Hero dispatch={dispatch} />
-
       {skillItems.map((item) => (
         <Skill key={item.title} item={item} dispatch={dispatch} />
       ))}
