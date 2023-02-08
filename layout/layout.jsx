a0bc8f9cb9c4def5795d81b721041db7f9469530
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-08 16:26:40 +0800

    forgot to uncomment nav

--- layout/layout.jsx
+++ layout/layout.jsx
@@ -4,7 +4,7 @@ import FixedNav from "./nav/nav";
 export default function Layout({ children, skillsNav }) {
   return (
     <>
-      {/* <FixedNav skillsNav={skillsNav} /> */}
+      <FixedNav skillsNav={skillsNav} />
       <main>{children}</main>
       <Footer />
     </>
