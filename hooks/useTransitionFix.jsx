42c846195ca9a1a4060564b2b3249dc6c4979c56
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-08 16:11:32 +0800

    transition fix test

---
+++ hooks/useTransitionFix.jsx
@@ -0,0 +1,31 @@
+import Router from "next/router";
+import { useEffect } from "react";
+
+export const OPACITY_EXIT_DURATION = 1;
+
+const routeChange = () => {
+  const tempFix = () => {
+    const elements = document.querySelectorAll('style[media="x"]');
+    elements.forEach((elem) => elem.removeAttribute("media"));
+    setTimeout(() => {
+      elements.forEach((elem) => elem.remove());
+    }, OPACITY_EXIT_DURATION * 1000);
+  };
+  tempFix();
+};
+
+export const useTransitionFix = () => {
+  useEffect(() => {
+    Router.events.on("routeChangeComplete", routeChange);
+    Router.events.on("routeChangeStart", routeChange);
+
+    return () => {
+      Router.events.off("routeChangeComplete", routeChange);
+      Router.events.off("routeChangeStart", routeChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    Router.router?.push(Router.router?.pathname);
+  }, []);
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
 import { MobileNavProvider } from "@/context/mobile-nav-context";
 import useSkillsNav from "@/hooks/useSkillsNav";
+import { useTransitionFix } from "@/hooks/useTransitionFix";
 import Layout from "@/layout/layout";
 import "@/styles/globals.css";
 import { Montserrat } from "@next/font/google";
 import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
-
-// temp import fix for styles unmounting abruptly
-
-import "@/components/home/hero/hero.module.css";
-import "@/components/home/about/about-item/about-item.module.css";
-import "@/components/home/contact/contact.module.css";
-import "@/components/home/work/work-item/work-item.module.css";
-import "@/components/home/hero/hero.module.css";
-
-import "@/components/skills/hero/hero.module.css";
-import "@/components/skills/skill/skill.module.css";
-import "@/components/skills/skill/tool/tool.module.css";
 
 let firstLoad = true;
 
@@ -25,6 +14,8 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export default function App({ Component, pageProps, router }) {
   const [isSmooth, setIsSmooth] = useState();
   const skillsNav = useSkillsNav();
+
+  useTransitionFix();
 
   useEffect(() => {
     router.events.on("routeChangeStart", () => setIsSmooth(false));
