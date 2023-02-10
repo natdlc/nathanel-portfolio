5113cf746a3f3e37de9acf9722dc355de47a47d5
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-02-10 20:42:38 +0800

    logo

--- layout/nav/header/header.jsx
+++ layout/nav/header/header.jsx
@@ -1,11 +1,19 @@
 import styles from "./header.module.css";
 import MobileNav from "./mobile-nav/mobile-nav";
 import MainMenu from "./main-menu/main-menu";
+import Image from "next/image";
+import logo from "@/public/logo.svg";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <p className={styles.logo}>nathanel.dev</p>
+      <Image
+        className={styles.logo}
+        src={logo}
+        alt="nathanel corpuz logo"
+        width={60}
+      />
+      {/* <p className={styles.logo}>nathanel.dev</p> */}
       <MobileNav />
       <MainMenu />
     </header>
